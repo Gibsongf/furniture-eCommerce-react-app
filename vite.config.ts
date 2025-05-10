ee9a14fdebdebb75c066682248de806d3099c276
Gibsongf <maccgiiibson@gmail.com>
2025-05-10 16:27:04 -0300

    Fix Vite base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    base: "/furniture-ecommerce-react-app/",
+    base: "/furniture-eCommerce-react-app/",
     plugins: [react()],
 });
