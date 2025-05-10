65b98851ac6df1827ce0c1651d0ac46b6fa0510d
Gibsongf <maccgiiibson@gmail.com>
2025-05-10 15:37:22 -0300

    Add 404.html and configuration for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+import { viteStaticCopy } from "vite-plugin-static-copy";
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/furniture-eCommerce-react-app/",
-    plugins: [react()],
+    base: "/furniture-eCommerce-react-app/", // importante para GitHub Pages
+    plugins: [
+        react(),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: "404.html", // arquivo de redirecionamento
+                    dest: ".", // será copiado para dist/404.html
+                },
+            ],
+        }),
+    ],
 });
