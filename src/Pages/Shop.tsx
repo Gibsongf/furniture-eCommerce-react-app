52260f4b32038c4bed3b6c3f30061d4798665dc8
Gibsongf <maccgiiibson@gmail.com>
2026-02-05 20:59:54 -0300

    shop header bg color with img in bg

--- src/Pages/Shop.tsx
+++ src/Pages/Shop.tsx
@@ -9,71 +9,68 @@ import { createContext, useRef, useState, RefObject } from "react";
 import { themePaletteColor } from "../components/Theme";
 
 interface ShopContextType {
-    itemsPerPage: string;
-    changeItemsCount: (event: string) => void;
-    sortBy: string;
-    setSortBy: (value: string) => void;
-    ref: RefObject<HTMLDivElement | undefined>;
+  itemsPerPage: string;
+  changeItemsCount: (event: string) => void;
+  sortBy: string;
+  setSortBy: (value: string) => void;
+  ref: RefObject<HTMLDivElement | undefined>;
 }
 export const ShopContext = createContext<ShopContextType>(
-    {} as ShopContextType
+  {} as ShopContextType,
 );
 
 export default function Shop() {
-    //specify the type as HTMLDivElement to avoid alert at scrollIntoView
-    const ProductViewAdjusterRef = useRef<HTMLDivElement>(null);
-    const [itemsPerPage, setItemPage] = useState("8");
-    const [page, setPage] = useState(1);
-    const [sortBy, setSortBy] = useState("descending");
-    const changeItemsCount = (value: string) => {
-        setItemPage(value);
-        setPage(1);
-    };
-    const pagesObj = pagination(itemsPerPage);
-    const handleChange = (
-        _event: React.ChangeEvent<unknown>,
-        value: number
-    ) => {
-        setPage(value);
-        ProductViewAdjusterRef.current?.scrollIntoView({ behavior: "smooth" });
-    };
-    return (
-        <>
-            {" "}
-            <ShopHeader pageName="Shop" />
-            {/* need filter or sort by and info of how many items is in the page */}
-            {/* the sort event should happen at grid product only in the item at the current page*/}
-            <ShopContext.Provider
-                value={{
-                    itemsPerPage,
-                    changeItemsCount,
-                    ref: ProductViewAdjusterRef,
-                    sortBy,
-                    setSortBy,
-                }}>
-                <ProductViewAdjuster />
-                <GridProducts
-                    itemsPerPage={itemsPerPage}
-                    products={pagesObj[String(page - 1)]}
-                />
-            </ShopContext.Provider>
-            <ThemeProvider theme={themePaletteColor}>
-                <Pagination
-                    count={Object.keys(pagesObj).length}
-                    shape="rounded"
-                    page={page}
-                    size="large"
-                    onChange={handleChange}
-                    sx={{
-                        "& .MuiPagination-ul": {
-                            justifyContent: "center",
-                        },
-                        margin: "30px",
-                    }}
-                    color="primary"
-                />
-            </ThemeProvider>
-            <ServiceBenefits />
-        </>
-    );
+  //specify the type as HTMLDivElement to avoid alert at scrollIntoView
+  const ProductViewAdjusterRef = useRef<HTMLDivElement>(null);
+  const [itemsPerPage, setItemPage] = useState("8");
+  const [page, setPage] = useState(1);
+  const [sortBy, setSortBy] = useState("descending");
+  const changeItemsCount = (value: string) => {
+    setItemPage(value);
+    setPage(1);
+  };
+  const pagesObj = pagination(itemsPerPage);
+  const handleChange = (_event: React.ChangeEvent<unknown>, value: number) => {
+    setPage(value);
+    ProductViewAdjusterRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+  return (
+    <>
+      {" "}
+      <ShopHeader pageName="Shop" />
+      {/* need filter or sort by and info of how many items is in the page */}
+      {/* the sort event should happen at grid product only in the item at the current page*/}
+      <ShopContext.Provider
+        value={{
+          itemsPerPage,
+          changeItemsCount,
+          ref: ProductViewAdjusterRef,
+          sortBy,
+          setSortBy,
+        }}>
+        {/* <ProductViewAdjuster /> */}
+        <GridProducts
+          itemsPerPage={itemsPerPage}
+          products={pagesObj[String(page - 1)]}
+        />
+      </ShopContext.Provider>
+      <ThemeProvider theme={themePaletteColor}>
+        <Pagination
+          count={Object.keys(pagesObj).length}
+          shape="rounded"
+          page={page}
+          size="large"
+          onChange={handleChange}
+          sx={{
+            "& .MuiPagination-ul": {
+              justifyContent: "center",
+            },
+            margin: "30px",
+          }}
+          color="primary"
+        />
+      </ThemeProvider>
+      {/* <ServiceBenefits /> */}
+    </>
+  );
 }
--- src/components/Shop/ShopHeader.tsx
+++ src/components/Shop/ShopHeader.tsx
@@ -1,31 +1,21 @@
 import { Box, Typography } from "@mui/material";
 import "../../../src/App.css";
 export const ShopHeader = ({ pageName }: { pageName: string }) => {
-    return (
-        <div
-            className="header-shop"
-            style={{
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "flex-end",
-                width: "99vw",
-                //to ignore gap of stack in app.tsx
-                // paddingTop: "-10px",
-            }}>
-            <Box
-                sx={{
-                    backgroundColor: "rgba(233, 233, 233, 0.6)",
-                    width: "100%",
-                    height: "100%",
-                }}
-                display="flex"
-                justifyContent="center"
-                alignItems="center"
-                flexWrap={"wrap"}>
-                <Typography fontWeight="500" variant="h3" component="h1">
-                    {pageName}
-                </Typography>
-            </Box>
-        </div>
-    );
+  return (
+    <Box
+      className="header-shop"
+      sx={{
+        backgroundColor: "rgba(233, 233, 233, 0.6)",
+        width: "100%",
+        minHeight: "300px",
+      }}
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      flexWrap={"wrap"}>
+      <Typography fontWeight="500" variant="h3" component="h1">
+        {pageName}
+      </Typography>
+    </Box>
+  );
 };
