f956b0002ee8dca6cba8267cb23051bec52d2362
Gibsongf <maccgiiibson@gmail.com>
2026-02-05 22:43:52 -0300

    benefit el cs media query

--- src/components/Shop/ServiceBenefits.tsx
+++ src/components/Shop/ServiceBenefits.tsx
@@ -5,85 +5,80 @@ import SupportAgentOutlinedIcon from "@mui/icons-material/SupportAgentOutlined";
 import { Box, Typography } from "@mui/material";
 
 export const ServiceBenefits = () => {
-    return (
+  return (
+    <Box
+      sx={{
+        backgroundColor: "#F9F1E7",
+        // height: "100px",
+      }}>
+      <Box
+        maxWidth={"1200px"}
+        display="flex"
+        alignItems="center"
+        margin="0 auto"
+        padding="20px"
+        justifyContent="space-between">
         <Box
-            display="flex"
-            alignItems="center"
-            justifyContent="space-around"
-            sx={{
-                backgroundColor: "#F9F1E7",
-                width: "99vw",
-                height: "100px",
-                padding: "20px 0px",
-            }}>
-            <Box display="flex" gap={1} alignItems="center">
-                <EmojiEventsOutlinedIcon sx={{ fontSize: 70 }} />
-                <Box textAlign="left">
-                    <Typography
-                        fontWeight="bold"
-                        variant="subtitle1"
-                        component="h2">
-                        Nullam quis
-                    </Typography>
-                    <Typography
-                        color="#898989"
-                        variant="subtitle1"
-                        component="h2">
-                        Maecenas tellus eget
-                    </Typography>
-                </Box>
-            </Box>
-            <Box display="flex" gap={1} alignItems="center">
-                <VerifiedOutlinedIcon sx={{ fontSize: 70 }} />
-                <Box textAlign="left">
-                    <Typography
-                        fontWeight="bold"
-                        variant="subtitle1"
-                        component="h2">
-                        Maecenas nec
-                    </Typography>
-                    <Typography
-                        color="#898989"
-                        variant="subtitle1"
-                        component="h2">
-                        Donec vitae sapien
-                    </Typography>
-                </Box>
-            </Box>
-            <Box display="flex" gap={1} alignItems="center">
-                <CardGiftcardOutlinedIcon sx={{ fontSize: 70 }} />
-                <Box textAlign="left">
-                    <Typography
-                        fontWeight="bold"
-                        variant="subtitle1"
-                        component="h2">
-                        Aenean imperdiet
-                    </Typography>
-                    <Typography
-                        color="#898989"
-                        variant="subtitle1"
-                        component="h2">
-                        Maecenas tellus eget
-                    </Typography>
-                </Box>
-            </Box>
-            <Box display="flex" gap={1} alignItems="center">
-                <SupportAgentOutlinedIcon sx={{ fontSize: 70 }} />
-                <Box textAlign="left">
-                    <Typography
-                        fontWeight="bold"
-                        variant="subtitle1"
-                        component="h2">
-                        Etiam rhoncus
-                    </Typography>
-                    <Typography
-                        color="#898989"
-                        variant="subtitle1"
-                        component="h2">
-                        Maecenas tellus eget
-                    </Typography>
-                </Box>
-            </Box>
+          className="benefit-container"
+          display="flex"
+          gap={1}
+          alignItems="center">
+          <EmojiEventsOutlinedIcon sx={{ fontSize: 70 }} />
+          <Box textAlign="left">
+            <Typography fontWeight="bold" variant="subtitle1" component="h2">
+              Nullam quis
+            </Typography>
+            <Typography color="#898989" variant="subtitle1" component="h2">
+              Maecenas tellus eget
+            </Typography>
+          </Box>
         </Box>
-    );
+        <Box
+          className="benefit-container"
+          display="flex"
+          gap={1}
+          alignItems="center">
+          <VerifiedOutlinedIcon sx={{ fontSize: 70 }} />
+          <Box textAlign="left">
+            <Typography fontWeight="bold" variant="subtitle1" component="h2">
+              Maecenas nec
+            </Typography>
+            <Typography color="#898989" variant="subtitle1" component="h2">
+              Donec vitae sapien
+            </Typography>
+          </Box>
+        </Box>
+        <Box
+          className="benefit-container hide-benefit"
+          display="flex"
+          gap={1}
+          alignItems="center">
+          <CardGiftcardOutlinedIcon sx={{ fontSize: 70 }} />
+          <Box textAlign="left">
+            <Typography fontWeight="bold" variant="subtitle1" component="h2">
+              Aenean imperdiet
+            </Typography>
+            <Typography color="#898989" variant="subtitle1" component="h2">
+              Maecenas tellus eget
+            </Typography>
+          </Box>
+        </Box>
+        <Box
+          className="benefit-container"
+          display="flex"
+          gap={1}
+          alignItems="center">
+          <SupportAgentOutlinedIcon sx={{ fontSize: 70 }} />
+          <Box textAlign="left">
+            <Typography fontWeight="bold" variant="subtitle1" component="h2">
+              Etiam rhoncus
+            </Typography>
+            <Typography color="#898989" variant="subtitle1" component="h2">
+              Maecenas tellus eget
+            </Typography>
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
 };
